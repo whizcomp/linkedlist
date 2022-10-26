2eac3be5254c06c49baa917fd699eb49c66780dc
Victor Kipkoech <ngenovic4@gmail.com>
2022-10-26 14:16:24 +0300

    linkedlist v2

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-class ListNode {
+class NodeList {
     constructor(data) {
         this.data = data;
-        this.next = null
+        this.next = null;
     }
 }
-
 class LinkedList {
     constructor(head = null) {
         this.head = head;
-
     }
     size() {
-        let count = 0;
+        let count = 0
         let node = this.head;
         while (node) {
             count++;
-            node = node.next;
+            node = node.next
         }
-        return count;
-    }
-    clear() {
-        this.head = null
+        return count
     }
     getLast() {
         let lastNode = this.head;
         while (lastNode.next) {
-            lastNode = lastNode.next;
+            lastNode = lastNode.next
         }
         return lastNode
     }
@@ -33,11 +28,10 @@ class LinkedList {
         return this.head
     }
 }
-node1 = new ListNode(2);
-node2 = new ListNode(5);
-node3 = new ListNode(3)
+const node1 = new NodeList(1);
+const node2 = new NodeList(7);
+const node3 = new NodeList(3)
 node1.next = node2;
-node2.next = node3;
-
-let list = new LinkedList(node1);
-console.log(list)
+node2.next = node3
+const list = new LinkedList(node1)
+console.log(list.getLast())
